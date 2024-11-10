c0a0c5b7445ce757c3d869403b55d702cc4c9bb5
Bilal Mohammed <kurdo1ka@gmail.com>
2024-11-10 21:29:20 +0100

    added a new File World.class.js

--- js/game.js
+++ js/game.js
@@ -1,14 +1,21 @@
 let canvas;
 let ctx;
-let character = new MovableObject();
+// let character = new Character();
 
+// let enemies = [
+//     new Chicken(),
+//     new Chicken(),
+//     new Chicken(),
+// ];
+
+let world = new World();
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
 
 
-    console.log('My charechter is', character);
+    console.log('My charechter is', world.character);
     
 
     // character.src = '../img/2_character_pepe/2_walk/W-21.png';
