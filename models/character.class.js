aab53b3e0f98e49bac3356e0482bb5f92f58cb33
Bilal Mohammed <kurdo1ka@gmail.com>
2024-11-13 15:46:48 +0100

    added walking RIGHT and LEFT to character

--- models/character.class.js
+++ models/character.class.js
@@ -2,6 +2,7 @@ class Character extends MovableObject {
 
     height = 350;
     y = 90;
+    speed = 10;
     IMAGES_WALKING = [
         'img/2_character_pepe/2_walk/W-21.png',
         'img/2_character_pepe/2_walk/W-22.png',
@@ -21,17 +22,30 @@ class Character extends MovableObject {
     }
 
     animate() {
+
+        setInterval(() => {
+            if (this.world.keyboard.RIGHT) {
+                this.x += this.speed;
+            }
+
+            if (this.world.keyboard.LEFT) {
+                this.x -= this.speed;
+            }
+        }, 1000 / 30);
+
         setInterval(() => {
 
-            if (this.world.keyboard.RIGHT) {
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                // this.x += this.speed;
 
 
+                // Walk animation
                 let i = this.currentImage % this.IMAGES_WALKING.length; // let i = 0 % 6; 
                 let path = this.IMAGES_WALKING[i];
                 this.img = this.imageCache[path];
                 this.currentImage++;
             }
-        }, 100);
+        }, 50);
     }
 
     jump() {
